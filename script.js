796b2fdb236ea491a804259f9dae1700f82635f1
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-05 21:56:35 -0400

    creating the initial variable in logic

---
+++ script.js
@@ -0,0 +1,5 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
