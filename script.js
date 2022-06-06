0353dffc12409783d7b4ea946ad2463bac7676df
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-05 22:02:33 -0400

    creating the paddle, bricks and ball properties

--- script.js
+++ script.js
@@ -3,3 +3,41 @@ const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+const brickRowCount = 9;
+const brickCOlumnCount = 5;
+const delay = 500; //delay to reset the game
+
+//create ball props
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+    visible: true
+};
+
+//create paddle props
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+    visible: true
+};
+
+//create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+};
